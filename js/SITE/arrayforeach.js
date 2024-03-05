4cbbdd09045a78db01c2c4078c5665e463a7a934
Ygor Renato <ygor.renato@hotmail.com>
2024-03-05 16:09:29 -0300

    Commit inicial

---
+++ js/SITE/arrayforeach.js
@@ -0,0 +1,15 @@
+var a = [10,20,30,40,50,60];
+
+for (let valor of a){
+    console.log(valor);
+
+}
+
+a.forEach(valor => console.log(valor));
+
+console
+
+a.forEach(function(valor,indice,Array){
+    console.log(array[indice]);
+
+})
